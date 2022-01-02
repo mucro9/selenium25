e180dc4002df108b4534364a0d095d90e639c8b5
mucro9 <mucht-ozr@hotmail.com>
2022-01-01 19:36:48 -0700

    selenium

---
+++ src/test/java/com/cydeo/tests/day1_Selenium_Intro/SeleniumTest.java
@@ -0,0 +1,16 @@
+package com.cydeo.tests.day1_Selenium_Intro;
+public class SeleniumTest {
+    public static void main(String[] args) {
+        //1- Set up the driver
+        //2- Create instance of the driver
+        //3- Use the driver instance to test selenium
+
+
+
+
+
+
+
+
+    }
+}
